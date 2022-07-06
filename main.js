3d3678f62ca3505e84f5e517aa1c3913da84f7db
Brian Heredia <herediabrian123@gmail.com>
2022-07-06 20:01:40 -0300

    Creacion de la seccion productos para aplicar DOM

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-/*Simulador en el que el usuario pueda ingresar a la pagina y 
-interactuar con los articulos de la tienda, seleccionar cuales y la forma de pago*/
-//Cuando ingresa se solicita el usuario y contraseña
+
+const lista = []
+const sumar = (a, b) => a + b;
+const multi = (a, b) => a * b;
+const sumaGrande = (a, b, c) => a + b + c;
+
+let talle = 0
+let cantidad = 0
+
+class Tienda{
+        constructor(nombre, precio, talle) {
+            this.nombre = nombre; 
+            this.precio = precio,
+            this.talle = talle = prompt("Ingrese su talle");
+        }
+}
+
+function preguntar() {
+        let preguntaCompra = prompt("Por el momento no tenemos mas stock del que podemos ofrecerle, mil disculpas!(Escriba ok para proceder al carrito")
+        
+        if(preguntaCompra == "ok") {
+                alert("Sera redirigido a su carrito") 
+                carrito()
+        }
+            
+}
+
 let usuario = prompt("Porfavor ingrese su usuario")
 let contraseña = prompt("Porfavor ingrese su contraseña")    
 
@@ -34,153 +58,184 @@ function mostraMenu(menu) {
         }
     }
     
-    mostraMenu(prompt("Porfavor elegir un producto \n1 Zapatillas \n2 Buscar" ))
-    
-//Creamos una funcion con las zapatillas junto a un if y un prompt
+mostraMenu(prompt("Porfavor elegir un producto \n1 Zapatillas \n2 Buscar" ))
 
-function noSabesQueBuscar(pregunta) {
 
-        const marcas = [
-                {nombre: "nike"},
-                {nombre: "zapasnike"},
-                {nombre: "adidas"},
-                {nombre: "basket zapatillas"},
-        ];
+function zapatillasMenu() {
         
-        dudas = marcas.map((opciones) => opciones.nombre);
-        alert("Estas son algunos de los terminos mas buscados \n" + dudas.join("  -  "))
-
-        pregunta = prompt("Porfavor ingrese uno de los terminos anteriores y se le filtrara la busqueda ")
-
-        respuesta1 = marcas.find((pregunta) => pregunta.nombre === "nike")
-        respuesta1 = alert(nike())
+        let zapatillasPregunta = prompt(`Seleccione su marca preferida \n1. Adidas \n2. Under Armour \n3. Nike`)
         
-        respuesta2 = marcas.find((pregunta) => pregunta.nombre === "zapasnike")
-        respuesta2 = alert(nike())
-
-        respuesta4 = marcas.find((pregunta) => pregunta.nombre === "basket zapatillas")
-        respuesta4 = alert(nike())
-        
-
-}
-
-function zapatillasMenu(zapas) {
-        
-        zapas = prompt("Elija su marca de preferencia😎 e ingrese un numero \n1 Nike \n2 Adidas \n3 Under Armour" )
-        
-        if(zapas == 1) {
-            nike()
+        if (zapatillasPregunta == "1") {
+                adidas()
+        } else if (zapatillasPregunta == "2") {
+                underArmour()
         } else {
-                alert("Seleccione una opcion")
+                alert("Seleccione un termino correcto")
         }
 }
+
     
-//Una funcion constructora para utilizar los elementos declarados de la misma y definir las caracteristicas de cada zapatilla junto a loselementos tanto nombre, precio y talle a eleccion
+//Function constructora para integrar los productos con respectivas caracteristicas
 
 
-function nike() {
-        
-        const sumar = (a, b) => a + b;
-        const multi = (a, b) => a * b;
-        let total = 0
-        let precio2 = 0
-         
-        class Tienda{
-                constructor(nombre, precio, talle) {
-                    this.nombre = nombre; 
-                    this.precio = precio,
-                    this.talle = talle = prompt("Ingrese su talle");
-                }   
-            }
-        
-        let talle = 0
-    
-        const productosNike = [
-            { id: 1, nombre: "Nike Precision V", precio: 35000},
-            { id: 2, nombre: "Nike LeBron Witness 6", precio: 32000},
+function adidas() {
+        const productosAdidas = [
+                { id: 1, nombre: "Hoops 3.0 Mid Classic Vintage", precio: 15500},
+                { id: 2, nombre: "Adidas Cross Em Up 5K", precio: 12000},
         ];
-    
-        let listaproductos = productosNike.map((opciones) => opciones.nombre);
-        alert("Las nuevas zapas ingresadas son " + "\n" + listaproductos.join(" , "))
 
-        let nike = prompt("Ingrese el modelo de preferencia \n1 Nike Precision V \n2 Nike LeBron Witness 6 \n3 Proceder al pago")
-        
-        switch(nike) {
-            case "1":
-                const nike1 = new Tienda("Nike Precision V", 35000, talle)
-                alert(`Este es el producto que usted eligio \nNombre : ${nike1.nombre} \n Su talle elegido es : ${nike1.talle}\n Precio $ : ${nike1.precio}`)
-                let cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
-                total = multi(cantidad, 35000)
-                precio1 = sumar(total, 0)
-                preguntar()
-                pagos()
-                             
-            break;
-            case "2":
-                const nike2 = new Tienda("Nike Lebron witness 6", 32000, talle) 
-                alert(`Este es el producto que usted eligio \nNombre : ${nike2.nombre} \n Su talle elegio es : ${nike2.talle}\n Precio $ : ${nike2.precio}`) 
-                cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
-                total = multi(cantidad, 32000)
-                precio2 = sumar(total, 0)
-                preguntar()
-                pagos()
-            break;
-            case "3":
-                totalNike = sumar(precio1, precio2)
-                let metodos = prompt(`Elija un plan de pago
-                \n1. Efectivo
-                \n2. Transferencia
-                \n3. Tarjeta`)
-                switch(metodos) {
-                        case "1":
-                                pregunta1 = parseInt(prompt(`El monto a abonar es ${totalNike} pesos`))
-                                alert("Su pago se a realizado correctamente")
-                        break;
-                        case "2":
-                                pregunta2 = prompt(`Aqui estan los datos de la cuenta bancaria a la que debe transferir el dinero
-                                \n #Banco galicia 01702046600000087865`)
-                        break;
+        listaproductos = productosAdidas.map((opciones) => opciones.nombre);
+        alert("Las nuevas zapas ingresadas son " + "\n" + listaproductos.join("\n"))
+
+        let adidasLista = prompt("Ingrese el modelo que mas le guste \n1 Hoops 3.0 Mid Classic Vintage \n2 Adidas Cross Em Up 5k \n3 Adidas Next level \n4 Adidas Trae Young 1")
+
+        switch(adidasLista) {
+                case "1":
+                        const adidas1 = new Tienda("Adidas Hoops 3.0", 15600, talle)
+                        alert(`Este es el producto que usted eligio \n Nombre : ${adidas1.nombre} \n Su talle elegido es : ${adidas1.talle}\n Precio $ : ${adidas1.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalAdidas1 = multi(cantidad, 15600)
+                        precioAdidasOne= sumar(totalAdidas1, 0)
+                        lista.push(`Modelo: ${adidas1.nombre} precio: ${precioAdidasOne} pesos`)
+                       
+                break;
+                case "2":
+                        const adidas2 = new Tienda("Adidas Cross Em Up 5K", 12000, talle)
+                      
+                        alert(`Este es el producto que usted eligio \nNombre : ${adidas2.nombre} \n Su talle elegido es : ${adidas2.talle} \n Precio $ : ${adidas2.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalAdidas2 = multi(cantidad, 12000)
+                        precioAdidasTwo= sumar(totalAdidas2, 0)
+                        lista.push(`Modelo: ${adidas2.nombre} precio: ${precioAdidasTwo} pesos`)
                         
-                        case "3":
-                                pregunta3 = prompt(`Seleccione alguna de las opciones con un numero del 1 al 3
-                                \n1- 3 Cuotas
-                                \n2- 6 Cuotas
-                                \n3- 12 Cuotas con interes`)
-                                alert("Se completo el pago de acuerdo a las cuotas seleccionadas")
-                        break;
-                        default: 
-                                
-                        break 
+                break;
+                case "3":
+                        const adidas3 = new Tienda("Adidas Next Level", 39500, talle)
+                        alert(`Este es el producto que usted eligio \nNonbre : ${adidas3.nombre} \n Su talle elegido es : ${adidas3.talle} \n Precio $ : ${adidas3.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalAdidas3 = multi(cantidad, 39500)
+                        precioAdidasThree= sumar(totalAdidas3, 0)
+                        lista.push(`Modelo: ${adidas3.nombre} precio: ${precioAdidasThree} pesos`)
+                       
+                break;
+                case "4":
+                        const adidas4 = new Tienda("Adidas Trae Young 1", 43300, talle)
+                        alert(`Este es el producto que usted eligio \nNombre : ${adidas4.nombre} \n Su talle elegido es : ${adidas4.talle} \n Precio $ : ${adidas4.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalAdidas4 = multi(cantidad, 12000)
+                        precioAdidasFour= sumar(totalAdidas4, 0)
+                        lista.push(`Modelo: ${adidas4.nombre} precio: ${precioAdidasFour} pesos`)
+                        
+                break;
+                default:
+                        alert("Elija un numero porfavor")
+                break;
                 }
-        }
-    
+        preguntar()
 }
 
 
-           
-//Funcion para preguntar si desea algo mas el comprador
-    
-function preguntar() {
-        let preguntaCompra = prompt("Desea comprar algo mas? En el caso de no puede proceder al carrito")
+function underArmour () {
+
+        const productosNuevos = [
+                { id: 1, nombre: "Under Armour Charged Spawn 3", precio: 32500},
+                { id: 2, nombre: "Charged Essential Under Armour", precio: 42300},
+                { id: 3, nombre: "Hoops 3.0 Mid Classic Vintage", precio: 15500},
+                { id: 4, nombre: "Adidas Cross Em Up 5K", precio: 12000},
+        ];
+
+        listaproductos = productosNuevos.map((opciones) => opciones.nombre);
+        alert("Las nuevas zapas ingresadas son " + "\n" + listaproductos.join("\n"))
         
-        if(preguntaCompra == "si") {
-            mostraMenu(prompt("Porfavor elegir un producto \n1 Zapatillas" ))
-        } else (preguntaCompra == "no") 
-            alert("Sera redirigido a su carrito dentro de la seccion nike") 
-            nike()
-            
+        let underLista = prompt("Ingrese el modelo que mas le guste \n1 Under Armour Charged Spawn 3 \n2 Charged Essential Under Armour \n3 Under Armour Spawn 3 ")
+
+        switch(underLista) {
+                case "1":
+                        const under1 = new Tienda ("Under Armour Charged Spawn 3", 32500, talle)
+                        alert(`Este es el producto que usted eligio \nNombre: ${under1.nombre} \n Su talle elegido es: ${under1.talle} \n Precio $ : ${under1.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalUnder1 = multi(cantidad, 32500)
+                        precioUnderOne = sumar(totalUnder1, 0)
+                        lista.push(`Modelo: ${under1.nombre} - precio: ${precioUnderOne} pesos`)
+                
+                break;
+                case "2":
+                        const under2 = new Tienda ("Charged Essential Under Armour", 42300, talle)
+                        alert(`Este es el producto que usted eligio \nNombre ${under2.nombre} \n Su talle elegido es: ${under2.talle} \n Precio $ : ${under2.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalUnder2 = multi(cantidad, 42300)
+                        precioUnderTwo = sumar(totalUnder2, 0)
+                        lista.push(`Modelo: ${under2.nombre} - precio: ${precioUnderTwo} pesos`)
+                break;
+                case "3":
+                        const under3 = new Tienda ("Under Armour Spawn 3", 45000, talle)
+                        alert(`Este es el producto que usted eligio \nNombre ${under3.nombre} \n Su talle elegido e: ${under3.talle} \n Precio $ : ${under3.precio}`)
+                        cantidad = parseInt(prompt(`Porfavor ingrese la cantidad de pares`))
+                        totalUnder3 = multi(cantidad, 42300)
+                        precioUnderTree = sumar(totalUnder3, 0)
+                        lista.push(`Modelo: ${under3.nombre} precio: ${precioUnderTree} pesos`)
+                break;
+                default:
+                        alert("Seleccione una opcion")
+                break;
+        }
+        preguntar()
 }
 
 
+function carrito() {
+        alert(`Esta es su lista de compras: \n ${lista}`)
+        pagos()
+}
 
+function pagos() {
+        let metodos = prompt(`Elija un plan de pago
+        \n1. Efectivo
+        \n2. Transferencia
+        \n3. Tarjeta`)
+        switch(metodos) {
+                case "1":
+                        alert("Su pago se a realizado correctamente")
+                break;
+                case "2":
+                        pregunta2 = prompt(`Aqui estan los datos de la cuenta bancaria a la que debe transferir el dinero
+                        \n #Banco galicia 01702046600000087865`)
+                break;
+                
+                case "3":
+                        pregunta3 = prompt(`Seleccione alguna de las opciones con un numero del 1 al 3
+                        \n1- 3 Cuotas
+                        \n2- 6 Cuotas
+                        \n3- 12 Cuotas con interes`)
+                        alert("Se completo el pago de acuerdo a las cuotas seleccionadas")
+                break;
+                default: 
+                        
+                break 
+        }   
+}
 
+//Manejo de DOM (Aclaro que imprime los precios del array lista1)
 
+let lista1 = [{ id: 1, nombre: "Under Armour Charged Spawn 3", precio: 32500},
+                { id: 2, nombre: "Charged Essential Under Armour", precio: 42300},
+                { id: 3, nombre: "Hoops 3.0 Mid Classic Vintage", precio: 15500},
+                { id: 4, nombre: "Adidas Cross Em Up 5K", precio: 12000},]
 
+let productosEnHtml = document.getElementById("productosZapas")
 
-
-
-
-
+for(const zapatillas of lista1){
+    
+        let variableOne = document.createElement("span")
+        
+        variableOne.innerHTML= 
+        
+        `<p> Numero de compra: ${zapatillas.id} </p>
+        <p> Modelo: ${zapatillas.nombre} </p>
+        <p> Precio: ${zapatillas.precio} </p>
+        `
+        productosEnHtml.append(variableOne)
+}
 
 
 
