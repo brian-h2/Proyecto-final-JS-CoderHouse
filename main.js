581f82bb9fec94065ffe6c1075853235b94b58ef
Brian Heredia <herediabrian123@gmail.com>
2022-07-11 16:38:04 -0300

    Eventos

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Array con informacion de los productos
+
 const lista = [
         {id: 1, nombre: "Adidas Hoops 3.0", precio: 15600, img: "../img/productos-venta/adidas1.jpg", talle: "Talle 12"},
         {id: 2, nombre: "Adidas Cross Em Up", precio: 12000, img: "../img/productos-venta/adidas2.jpg", talle: "Talle 12"},
@@ -9,16 +11,25 @@ const lista = [
         {id: 8, nombre: "Nike KD 13 Black/grey", precio: 41900, img: "../img/productos-venta/nike2.jpg",talle: "Talle 10"},
         {id: 9, nombre: "Air Jordan XXXVI SE", precio: 52900, img: "../img/productos-venta/nike4.jpg",talle: "Talle 14"},
         {id: 10, nombre: "Nike Renew Elevate 3", precio: 38100, img: "../img/productos-venta/nike5.jpg",talle: "Talle 11"},
-    ]  
+        {id: 11, nombre: "Kyrie Infinity By You", precio: 36000, img: "../img/productos-venta/nike6.jpg",talle: "Talle 13"},
+        {id: 12, nombre: "Air Jordan XXXVI", precio: 32000, img: "../img/productos-venta/nike7.jpg",talle: "Talle 15"},
+        {id: 13, nombre: "Air Jordan XXXVI FS", precio: 42300, img: "../img/productos-venta/nike8.jpg",talle: "Talle 11"},
+        {id: 14, nombre: "Kyrie Infinity", precio: 32300, img: "../img/productos-venta/nike9.jpg",talle: "Talle 11"},
+        {id: 15, nombre: "Nike Air Zoom G.T", precio: 52300, img: "../img/productos-venta/nike10.jpg",talle: "Talle 11"},
 
-   
-let contenedorProductos = document.getElementById("tarjetas-compra")   
+]  
+
+//Constantes globales
+  
+const contenedorProductos = document.getElementById("tarjetas-compra")   
 
 const carritoLista = document.getElementById("carrito-lista") 
 
 const precioFinal = document.getElementById("precioTotal")
 
 let carrito = []
+
+
 
 //Creamos una iteracion para que se haga una card por cada objeto que se agregue al array lista 
 
@@ -44,7 +55,9 @@ for(const producto of lista) {
         
         boton.addEventListener('click', () => {
                 agregarCarrito(producto.id)
+                alert(`Su producto fue agregado al carrito`)
         })
+
 }
 
 //Arrow function para pushear el elemento que se elija a la variable carrito
@@ -72,11 +85,26 @@ const mostrarProducto = () => {
         
         carrito.forEach((producto) => {
         const product = document.createElement('div')
+        product.classList.add(`carrito`)
         product.innerHTML = `
-        <h5>Nombre del producto: ${producto.nombre}</h5>
-        <p>Precio: ${producto.precio}</p>
-        <button onclick="eliminarProducto(${producto.id})" = class="boton-eliminar"><i class="fas fa-trash-alt">Quitar</button>
-        `
+                                                                                        
+        <table class="table">
+                <thead>
+                        <tr>
+                                <th>Producto</th>
+                                <th>Nombre del producto</th>
+                                <th>Precio</th>
+                        </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                                <th><img src=${producto.img} alt="" style="width: 100px; height: 100px; background-color: #F0F0F0" class="imagen-carrito"></th>
+                                <td>${producto.nombre}</td>
+                                <td>${producto.precio} <button onclick="eliminarProducto(${producto.id})" = class="custom-btn btn"><i class="fas fa-trash-alt">Eliminar Producto</button></td>
+                        </tr>
+                </tbody>
+        </table>
+                                               `
         carritoLista.appendChild(product)
         })
 
@@ -84,8 +112,17 @@ const mostrarProducto = () => {
 }
 
 
+ /*const filtrado = document.getElementById("button-filter")
+        filtrado.addEventListener('click', () => {
+                precios.innerText =  lista.filter(producto => {
+                        return producto.precio <= 52900;
+                })
+})*/
 
 
-    
-    
+
+       
+
+
+ 
     
